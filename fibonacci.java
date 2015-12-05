8a7dbdac168a8557d72a2747bc601ce8770c9042
tiwarirs <radhikatiwari1708@gmail.com>
2015-12-05 13:07:06 +0530

    errors removed

--- fibonacci.java
+++ fibonacci.java
@@ -4,13 +4,12 @@
 public class goldenRatio {
 public static void main(String[] args) {
 System.out.println("The Golden Ratio:");//fibonacci series
-long i=0,j=1,k=0,l;
+long i=0,j=1,k,l;
 System.out.print(i+","+j);
-for(l=0;l<100;l++){
+for(l=2;l<10;++l){
 k=i+j;
 System.out.print(","+k);
 i=j;
 j=k;
 }
-System.out.println(" ...");
 }
